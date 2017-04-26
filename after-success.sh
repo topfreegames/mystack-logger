281378efc5f36af33bd41089d990902c701862e3
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-04-26 19:08:32 -0300

    fix after-success.sh

--- after-success.sh
+++ after-success.sh
@@ -2,12 +2,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 REPO=quay.io/topfreegames/mystack-logger
+DOCKER_REGISTRY=${DOCKER_REGISTRY:=quay.io}
 
 make build-docker
 
+if [ -v $DOCKER_USER ] || [ -v $DOCKER_PASSWORD ]; then
+  docker login --username="$DOCKER_USER" --password="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+fi
+
 # If this is not a pull request, update the branch's docker tag.
 if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
-  docker tag myapp $REPO:${TRAVIS_BRANCH/\//-} \
+  docker tag mystack-logger:latest $REPO:${TRAVIS_BRANCH/\//-} \
     && docker push $REPO:${TRAVIS_BRANCH/\//-};
 
   # If this commit has a tag, use on the registry too.
